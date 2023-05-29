e2fbb751c696c410a204c97aaf5d28fba8c5be95
Dev-001 <Dev-001@Forges-MacBook-Pro.local>
2023-05-29 11:41:54 -0700

    Api ResponseBuilder trait created

---
+++ app/Traits/ResponseBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+trait ResponseBuilder
+{
+    public function response(bool $status, int $code, string $message, string $description = null, array $data = null): JsonResponse
+    {
+        return response()->json([
+            'status' => $status,
+            'code' => $code,
+            'message' => $message,
+            'description' => $description,
+            'meta' => [
+                'version' => '1.0',
+                'timestamp' => now()->toDateTime(),
+            ],
+            'data' => $data,
+        ]);
+    }
+}
